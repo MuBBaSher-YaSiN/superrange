8b3a17cfebd502d0abebbb05a59d23a621dd7d89
Fatima_Iqra <iqraf683@gmail.com>
2025-08-12 00:13:32 +0500

    Install tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Metadata } from "next";
 import Wrapper from "@/layouts/Wrapper";
 import HomeOne from "@/components/homes/home";
-
+import { GrWhatsapp } from "react-icons/gr";
 export const metadata: Metadata = {
   title: 'Car Repair & Auto Services Next js Template',
   description: 'The official Next.js Course Dashboard, built with App Router.',
@@ -13,6 +13,9 @@ const index = () => {
 	return (
 		<Wrapper>
 			<HomeOne />
+			<span className='fixed h-44 bottom-6 right-4 z-[11111119] w-44 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors duration-300 cursor-pointer'>
+								<GrWhatsapp />
+							</span>
 		</Wrapper>
 	);
 };
--- src/common/ScrollToTop.tsx
+++ src/common/ScrollToTop.tsx
@@ -2,7 +2,7 @@
 
 import UseSticky from "@/hooks/UseSticky";
 import React, { useState, useEffect } from "react";
-
+// import { GrWhatsapp } from "react-icons/gr";
 
 const ScrollToTop = () => {
 	const { sticky }: { sticky: boolean } = UseSticky();
@@ -27,8 +27,10 @@ const ScrollToTop = () => {
 	}, []);
 
 	return (
-		<>  
-
+	<>  
+{/* // <span className='fixed h-44 bottom-2 right-4 z-auto w-44 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors duration-300 cursor-pointer'>
+// 					<GrWhatsapp />
+// 				</span> */}
 			<span className={`ak-scrollup ${sticky && "ak-scrollup-show"}`} onClick={scrollTop}>
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 10L1.7625 11.7625L8.75 4.7875V20H11.25V4.7875L18.225 11.775L20 10L10 0L0 10Z" fill="currentColor"></path>
